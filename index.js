975365139591d5f83fd606c0228452455a012194
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-11 08:03:36 -0800

    commit tree ce669221ec25e248572a9bf99786f9980237272e

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    console.log(activity);
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bikeride')
